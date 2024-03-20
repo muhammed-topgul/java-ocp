dba2cf8e7f1b580b2d3e4f5b95ce229f14bf83da
mtopgul <muhammedtopgul.29@gmail.com>
2024-03-20 16:51:33 +0300

    > Chapter: 11-Exceptions and Localization

--- src/main/java/com/mtopgul/Util.java
+++ src/main/java/com/mtopgul/Util.java
@@ -1,5 +1,9 @@
 package com.mtopgul;
 
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+
 /**
  * @author muhammed-topgul
  * @since 15/02/2024 09:20
@@ -18,5 +22,13 @@ public final class Util {
 
     public static void divide(String title, int newLineCount) {
         System.out.println("\n".repeat(newLineCount) + "----------" + title + "----------");
+    }
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Day of Year = " + today.getDayOfYear());
+
+        int weekOfYear = today.get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear());
+        System.out.println("Week of Year = " + weekOfYear);
     }
 }
