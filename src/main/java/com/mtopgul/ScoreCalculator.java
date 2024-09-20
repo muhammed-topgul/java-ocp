814eb9d21f53ce68a8ce7452f7708e2bf71e6a07
muhammed-topgul <muhammedtopgul.29@gmail.com>
2024-09-21 01:55:19 +0300

    > Chapter: 05-Methods   # Exam#01

--- src/main/java/com/mtopgul/ScoreCalculator.java
+++ src/main/java/com/mtopgul/ScoreCalculator.java
@@ -5,30 +5,30 @@ import java.util.Arrays;
 public class ScoreCalculator {
     public static void main(String[] args) {
         int[] options = {
-               1,
-               3,
-               3,
-               3,
-               1,
-               1,
-               2,
-               3,
-               3,
-               1,
-               1,
-               3,
-               1,
                2,
                2,
+               2,
+               4,
+               3,
                3,
                2,
+               1,
+               4,
+               1,
                2,
+               1,
+               1,
+               1,
+               1,
+               1,
+               1,
+               3,
                3,
                2,
                2,
         };
 
-        int wrongOptions = 5;
+        int wrongOptions = 2;
         int sum = Arrays.stream(options).sum();
         double score = ((double) 100 / sum) * (sum - wrongOptions);
         System.out.printf("Score: %.2f", score);
