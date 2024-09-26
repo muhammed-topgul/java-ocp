3d0aa5f3da1d604810902da2a77fe4b39f1404e2
mtopgul <muhammedtopgul.29@gmail.com>
2024-09-26 09:48:04 +0300

    > Chapter: 08-Lambdas and Functional Interfaces   # Exam#01

--- src/main/java/com/mtopgul/ScoreCalculator.java
+++ src/main/java/com/mtopgul/ScoreCalculator.java
@@ -5,39 +5,30 @@ import java.util.Arrays;
 public class ScoreCalculator {
     public static void main(String[] args) {
         int[] options = {
-                2,
-                4,
-                1,
                 1,
                 1,
                 2,
-                1,
+                2,
                 3,
+                2,
+                1,
+                1,
+                2,
                 3,
-                4,
-                1,
-                3,
-                1,
-                4,
                 1,
                 1,
+                1,
+                2,
                 2,
                 1,
                 1,
                 3,
                 1,
+                1,
                 3,
-                1,
-                2,
-                1,
-                2,
-                4,
-                3,
-                1,
-                2,
         };
 
-        int wrongOptions = 7;
+        int wrongOptions = 1;
         int sum = Arrays.stream(options).sum();
         double score = ((double) 100 / sum) * (sum - wrongOptions);
         System.out.printf("Score: %.2f", score);
