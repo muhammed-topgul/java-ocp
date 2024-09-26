1aee994d5c2b0a43e72d1a83972542a98f3ac3be
mtopgul <muhammedtopgul.29@gmail.com>
2024-09-26 17:52:39 +0300

    > Chapter: 09-Collections and Generics   # Exam#01

--- src/main/java/com/mtopgul/ScoreCalculator.java
+++ src/main/java/com/mtopgul/ScoreCalculator.java
@@ -5,30 +5,29 @@ import java.util.Arrays;
 public class ScoreCalculator {
     public static void main(String[] args) {
         int[] options = {
-                1,
-                1,
                 2,
                 2,
-                3,
-                2,
-                1,
                 1,
                 2,
-                3,
                 1,
-                1,
-                1,
-                2,
                 2,
                 1,
                 1,
                 3,
+                4,
+                2,
                 1,
                 1,
-                3,
+                2,
+                2,
+                1,
+                2,
+                1,
+                1,
+                3
         };
 
-        int wrongOptions = 1;
+        int wrongOptions = 6;
         int sum = Arrays.stream(options).sum();
         double score = ((double) 100 / sum) * (sum - wrongOptions);
         System.out.printf("Score: %.2f", score);
