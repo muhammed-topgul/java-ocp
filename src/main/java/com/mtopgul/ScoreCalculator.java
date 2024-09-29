1dcc29bc7007d64b885fe26fb544271ef3dcc53b
mtopgul <muhammedtopgul.29@gmail.com>
2024-09-29 17:41:06 +0300

    > Chapter: 10-Streams   # Exam#01

--- src/main/java/com/mtopgul/ScoreCalculator.java
+++ src/main/java/com/mtopgul/ScoreCalculator.java
@@ -5,29 +5,29 @@ import java.util.Arrays;
 public class ScoreCalculator {
     public static void main(String[] args) {
         int[] options = {
+                1,
+                1,
+                1,
                 2,
                 2,
                 1,
-                2,
                 1,
                 2,
-                1,
+                2,
                 1,
                 3,
-                4,
-                2,
                 1,
                 1,
                 2,
                 2,
-                1,
                 2,
+                1,
                 1,
                 1,
                 3
         };
 
-        int wrongOptions = 6;
+        int wrongOptions = 5;
         int sum = Arrays.stream(options).sum();
         double score = ((double) 100 / sum) * (sum - wrongOptions);
         System.out.printf("Score: %.2f", score);
