4da0dcba95ed72976aadb4eb06433c219da9ad8d
mtopgul <muhammedtopgul.29@gmail.com>
2024-09-30 07:44:46 +0300

    > Score calculation method updated.

--- src/main/java/com/mtopgul/ScoreCalculator.java
+++ src/main/java/com/mtopgul/ScoreCalculator.java
@@ -1,35 +1,51 @@
 package com.mtopgul;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+/**
+ * @author muhammed-topgul
+ * @since 29/09/2024 18:00
+ */
 public class ScoreCalculator {
-    public static void main(String[] args) {
-        int[] options = {
-                1,
-                1,
-                1,
-                2,
-                2,
-                1,
-                1,
-                2,
-                2,
-                1,
-                3,
-                1,
-                1,
-                2,
-                2,
-                2,
-                1,
-                1,
-                1,
-                3
-        };
+    private final static Scanner scanner = new Scanner(System.in);
+    private static int questionsCount = 0;
+    private static int correctAnswerCount = 0;
+    private static double partialTotal = 0;
 
-        int wrongOptions = 5;
-        int sum = Arrays.stream(options).sum();
-        double score = ((double) 100 / sum) * (sum - wrongOptions);
-        System.out.printf("Score: %.2f", score);
+    public static void main(String[] args) {
+        System.out.print("Exam Name: ");
+        String examName = scanner.nextLine();
+        calculate(examName);
+    }
+
+    private static void calculate(String examName) {
+        Predicate<String> correct = s -> s.startsWith("✅");
+        Predicate<String> partially = s -> s.startsWith("☑\uFE0F");
+        Predicate<String> wrong = s -> s.startsWith("❌");
+        Predicate<String> questions = correct.or(wrong).or(partially);
+
+        InputStream fileResource = ClassLoader.getSystemResourceAsStream("exams/" + examName);
+        Stream<String> stream = new BufferedReader(new InputStreamReader(fileResource)).lines();
+        stream.filter(questions)
+                .forEach(s -> {
+                    questionsCount++;
+                    if (correct.test(s)) {
+                        correctAnswerCount++;
+                    } else if (partially.test(s)) {
+                        int length = s.length();
+                        int beginIndex = s.indexOf("(");
+                        String optionsCount = s.substring(beginIndex + 1, beginIndex + 2);
+                        String correctCount = s.substring(length - 2, length - 1);
+                        partialTotal += (double) Integer.parseInt(correctCount) / Integer.parseInt(optionsCount);
+                    }
+                });
+        double perQuestionRate = 100.0 / questionsCount;
+        double total = (perQuestionRate * correctAnswerCount) + (partialTotal * perQuestionRate);
+        System.out.printf("Total: %.2f",  total);
     }
 }
