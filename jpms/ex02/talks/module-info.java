127e2ebdd41887404193f91765ab26f8a109f9aa
muhammed-topgul <muhammedtopgul.29@gmail.com>
2025-04-21 23:22:27 +0300

    > Chapter: 12-Modules

--- jpms/ex02/talks/module-info.java
+++ jpms/ex02/talks/module-info.java
@@ -1,1 +1,8 @@
-module zoo.animal.talkls
+module zoo.animal.talks {
+    exports zoo.animal.talks.content;
+    exports zoo.animal.talks.media;
+    exports zoo.animal.talks.schedule;
+
+    requires zoo.animal.feeding;
+    requires zoo.animal.care;
+}
