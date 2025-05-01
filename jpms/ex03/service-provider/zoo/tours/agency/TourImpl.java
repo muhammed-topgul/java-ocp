5062f263bb9e222e47f3636d6a679d4e4a68994b
muhammed-topgul <muhammedtopgul.29@gmail.com>
2025-05-02 00:27:35 +0300

    > Chapter: 12-Modules

---
+++ jpms/ex03/service-provider/zoo/tours/agency/TourImpl.java
@@ -0,0 +1,17 @@
+package zoo.tours.agency;
+
+import zoo.tours.api.*;
+
+public class TourImpl implements Tour {
+    public String name() {
+        return "Behind the Scenes";
+    }
+
+    public int length() {
+        return 120;
+    }
+
+    public Souvenir getSouvenir() {
+        return new Souvenir("stuffed animal");
+    }
+}
